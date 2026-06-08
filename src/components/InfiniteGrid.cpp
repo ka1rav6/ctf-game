882830c7f9f826be5c82d466af3dfb8f54af0e49
Kairav <kairavdutta@gmail.com>
2026-06-09 00:27:02 +0530

    Made grid functions

--- src/components/InfiniteGrid.cpp
+++ src/components/InfiniteGrid.cpp
@@ -1,5 +1,30 @@
-//
-// Created by kairav on 6/8/26.
-//
-
 #include "InfiniteGrid.h"
+
+InfiniteGrid::InfiniteGrid(){
+    shader = new Shader("../src/components/shaders/InfiniteGrid.vs","../src/components/shaders/InfiniteGrid.fs");
+    std::cout << "InfiniteGrid created" << std::endl;
+    LOG_INFO("Shader for infinite grid created");
+    glGenVertexArrays(1, &VAO);
+}
+
+InfiniteGrid::~InfiniteGrid(){
+    delete shader;
+    shader = nullptr;
+    glDeleteVertexArrays(1, &VAO);
+}
+
+void InfiniteGrid::render(Camera& camera, const glm::mat4& projection, float nearPlane, float farPlane){
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    LOG_INFO("Infinite Grid rendering now");
+    shader->use();
+    shader->setMat4("view",camera.GetViewMatrix());
+    shader->setMat4("projection", projection);
+    shader->setFloat("nearPlane", nearPlane);
+    shader->setFloat("farPlane", farPlane);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindVertexArray(0);
+
+    glDisable(GL_BLEND);
+}
