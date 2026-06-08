3f64f4c0f2c8a4301dc9e060399cfa82ece7e18d
Kairav <kairavdutta@gmail.com>
2026-06-08 23:41:07 +0530

    Created infiniteGrid class for the land

---
+++ src/components/InfiniteGrid.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kairav on 6/8/26.
+//
+
+#include "InfiniteGrid.h"
---
+++ src/components/InfiniteGrid.h
@@ -0,0 +1,20 @@
+//
+// Created by kairav on 6/8/26.
+//
+
+#ifndef MAIN_GAME_INFINITEGRID_H
+#define MAIN_GAME_INFINITEGRID_H
+#include "../include/common.h"
+
+class InfiniteGrid {
+public:
+    InfiniteGrid();
+    ~InfiniteGrid();
+    void render(Camera& camera, const glm::mat4& projection, float nearPlane, float farPlane);
+private:
+    Shader* shader = nullptr;
+    unsigned int VAO;   // empty VAO
+};
+
+
+#endif //MAIN_GAME_INFINITEGRID_H
