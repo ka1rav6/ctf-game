ef9d9638112075bbb77e3136739a2434c7b5c8d8
Kairav <kairavdutta@gmail.com>
2026-06-09 12:59:06 +0530

    Created a basic wrapper over entt with the help of a templated entity class

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,5 +1,28 @@
 //
 // Created by kairav on 6/9/26.
 //
-
 #include "../include/Entity.h"
+#include "../include/Scene.h"
+#include "../include/common.h"
+bool Entity::valid() const{
+    return scene &&
+           scene->reg.valid(handle);
+}
+template<typename T, typename... Args>
+T& Entity::addComponent(Args&&... args){
+    return scene->reg.emplace<T>(
+        handle,
+        std::forward<Args>(args)...);
+}
+template<typename T>
+T& Entity::getComponent(){
+    return scene->reg.get<T>(handle);
+}
+template<typename T>
+bool Entity::hasComponent(){
+    return scene->reg.all_of<T>(handle);
+}
+template<typename T>
+void Entity::removeComponent(){
+    scene->reg.remove<T>(handle);
+}
--- src/Scene.cpp
+++ src/Scene.cpp
@@ -2,4 +2,15 @@
 // Created by kairav on 6/9/26.
 //
 
-#include "Scene.h"
+#include "../include/Scene.h"
+#include "../include/Entity.h"
+Scene::Scene() {
+    // auto entity = reg.create();
+    
+
+}
+
+Entity Scene::createEntity(){
+    auto e = reg.create();
+    return Entity(e, this);
+}
