d5ecc25c199b40d5bde2dcf913a951fb8a2fc793
Kairav <kairavdutta@gmail.com>
2026-06-09 12:50:31 +0530

    Created basic components that can be attached to entities

--- include/Entity_Components.h
+++ include/Entity_Components.h
@@ -5,4 +5,25 @@
 #ifndef MAIN_GAME_ENTITY_COMPONENTS_H
 #define MAIN_GAME_ENTITY_COMPONENTS_H
 
+#include "../include/common.h"
+
+struct Transform{
+    glm::vec3 position{0.0f};
+    glm::vec3 rotation{0.0f};
+    glm::vec3 scale{1.0f};
+};
+
+struct MeshRenderer{
+    std::string meshPath;
+};
+
+struct Rigidbody{
+    float mass = 1.0f;
+};
+struct Collider{};
+struct Physics{};
+struct Light{};
+struct Sprite{};
+struct Audio{};
+
 #endif //MAIN_GAME_ENTITY_COMPONENTS_H
