6b624f5910343946c08fe2c923c3ba65d23c53ad
Kairav <kairavdutta@gmail.com>
2026-06-11 12:48:25 +0530

    Created basic phyengine class and also added the reactengine to build paths

--- include/systems/PhysicsEngine.h
+++ include/systems/PhysicsEngine.h
@@ -5,9 +5,23 @@
 #ifndef MAIN_GAME_PHYSICSENGINE_H
 #define MAIN_GAME_PHYSICSENGINE_H
 
+// PhysicsSystem.h
+
+#include <reactphysics3d/reactphysics3d.h>
+
+namespace reactphysics3d {
+    class PhysicsWorld;
+}
 
 class PhysicsEngine {
+public:
+    reactphysics3d::PhysicsCommon physicsCommon;
+    reactphysics3d::PhysicsWorld* world;
+
+    PhysicsEngine();
+    void update(float dt);
 };
+
 
 
 #endif //MAIN_GAME_PHYSICSENGINE_H
