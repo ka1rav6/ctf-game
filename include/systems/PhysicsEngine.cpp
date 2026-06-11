46bb5fac1df23d6dd5bbb65e4701c3e6dbeece9f
Kairav <kairavdutta@gmail.com>
2026-06-11 12:48:07 +0530

    Created basic phyengine class and also added the reactengine to build paths

---
+++ include/systems/PhysicsEngine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kairav on 6/11/26.
+//
+
+#include "PhysicsEngine.h"
---
+++ include/systems/PhysicsEngine.h
@@ -0,0 +1,13 @@
+//
+// Created by kairav on 6/11/26.
+//
+
+#ifndef MAIN_GAME_PHYSICSENGINE_H
+#define MAIN_GAME_PHYSICSENGINE_H
+
+
+class PhysicsEngine {
+};
+
+
+#endif //MAIN_GAME_PHYSICSENGINE_H
